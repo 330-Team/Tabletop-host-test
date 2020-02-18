334a25f124bf6a3a92fb3a83421d106e54cac9bc
Andrew Bridgeman <andrewbridgeman@dhcp-10-105-32-240.wireless.northwestern.private>
2020-02-17 22:16:38 -0600

    new version

--- js/races.js
+++ js/races.js
@@ -1,16 +1,61 @@
-// d3.json("races/elf-high.json", function(json) {
-//     console.log(json);
-// })
+let elf = "";
+let dwarf = "";
+let data = "";
+let human = "";
 
-const race_click = () =>{
-  document.getElementById("info").style = "display:normal";
-}
-
-
-fetch('../races/elf-high.json')
+fetch('races/elf-high.json')
   .then((response) => {
     return response.json();
   })
   .then((myJson) => {
-    console.log(myJson)
-  })
+    elf = myJson;
+  });
+
+fetch('races/dwarf.json')
+    .then((response) => {
+      return response.json();
+    })
+    .then((myJson) => {
+      dwarf = myJson;
+    });
+
+  fetch('races/human.json')
+        .then((response) => {
+          return response.json();
+        })
+        .then((myJson) => {
+          human = myJson;
+        });
+
+
+const race_click = (race) =>{
+  let races = document.getElementsByClassName("race");
+  for(i = 0; i<races.length; i++){
+    races[i].value = "0";
+  }
+  if(race == "elf"){
+    data = elf;
+  }
+  if(race == "dwarf"){
+    data = dwarf;
+  }
+  if(race == "human"){
+    data = human;
+  }
+  let race_box = document.getElementById(race);
+  let race_info = document.getElementById("info");
+  if(race_box.value == "0"){
+    race_info.innerHTML = "";
+    race_box.value = "1";
+    race_info.style = "display:normal";
+
+    Object.keys(data.stats).forEach(function (key) {
+      race_info.innerHTML += data.stats[key] + "<br />";
+    });
+
+    Object.keys(data.attributes).forEach(function (key) {
+      Object.keys(data.attributes[key]).forEach(function (key2) {
+        race_info.innerHTML += data.attributes[key][key2] + "<br />";
+      });});
+    }
+}
